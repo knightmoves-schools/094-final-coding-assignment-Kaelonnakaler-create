0641ddb796d47c902f29570e8729965d8b5665ea
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-04-16 14:20:39 -0500

    Add CSS variables and styles for lists

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Put your code here
+<title>CSS Variables</title>
+<style>
+  :root {
+--text-size: 200px;
+--length-size: 300px;
+--width-size: 150px;
+--text-padding: 10px;
+--narrow-text: 15px;
+}
+<title>Lists</title>
+<style>
+  
+  .bullet-point {
+list-style-type: coffee;
